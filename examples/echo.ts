a95d656a5745b1a87961093d0e032e9a719a3249
Eric Kidder <eric@kidderberg.us>
2022-06-29 20:35:56 -0400

    Moved the examples directory out of src

--- examples/echo.ts
+++ examples/echo.ts
@@ -1,4 +1,4 @@
-import { TelnetServer, TelnetConnection } from "../index";
+import { TelnetServer, TelnetConnection } from "../src/index";
 
 async function echo(conn: TelnetConnection) {
   const remote = `${conn.socket.remoteAddress}:${conn.socket.remotePort || "*"}`;
